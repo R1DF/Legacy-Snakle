e7947b07a1bf319366e100d8294b1c1913318ab0
R1DF <33981824+R1DF@users.noreply.github.com>
2022-02-27 12:47:42 +0600

    appended toml files (conf+theme)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 # Imports
-from tkinter import Tk # window GUI
-from config_loader import ConfigLoader, CONF_FILE, CONF_PATH
+from tkinter import Tk, messagebox # window GUI
+from config_loader import ConfigLoader
+from theme_loader import ThemeLoader
+from necessary_defaults import *
+from main_menu import *
 
 # Making the window
 class App(Tk):
@@ -9,10 +12,29 @@ class App(Tk):
         Tk.__init__(self)
         self.title("Snakle")
 
-        # Adjusting to settings
+        # Getting configurations
         self.conf = ConfigLoader(CONF_PATH+CONF_FILE)
-        self.geometry(self.conf.get("window")["width"] + "x" + self.conf.get("window")["height"])
-        self.resizable(False, False) # must change to f-string
+        self.WIDTH = self.conf.get("window")["width"]
+        self.HEIGHT = self.conf.get("window")["height"]
+        self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+        self.resizable(False, False)
+
+        # Getting theme data
+        self.theme_loader = ThemeLoader(self)
+
+        # Loading protocol
+        self.protocol("WM_DELETE_WINDOW", self.handle_exit)
+
+        # Making the main menu
+        self.make_main_menu()
+
+    def make_main_menu(self):
+        self.main_menu = MainMenu(self, self.theme_loader.load_theme(THEMES_PATH+DEFAULT_THEME), self.conf)
+        self.main_menu.pack(expand=1, fill="both")
+
+    def handle_exit(self):
+        if messagebox.askyesno("Confirm exit", "Are you sure you want to exit?"):
+            quit()
         
 
 # Creating the window instance
