d17944be0b938b0cb2f6db5293ac83210783af3d
R1DF <33981824+R1DF@users.noreply.github.com>
2022-02-27 12:49:10 +0600

    adapted for more features

--- src/screen.py
+++ src/screen.py
@@ -2,9 +2,25 @@
 from tkinter import Canvas, Tk
 
 # Screen template
-class Screen(Canvas):
-    def __init__(self, master):
+class Screen(Canvas,):
+    def __init__(self, master, theme: dict, conf):
         self.master = master # make master available to class
+        self.theme = theme["data"]
+        self.theme_metadata = theme["meta"]
+
+        # Initializing variables to not write too much code
+        self.conf = conf
+        self.WIDTH = self.conf.get("window")["width"]
+        self.HEIGHT = self.conf.get("window")["height"]
+        self.FONT_FAMILY = self.conf.get("text")["font_family"]
+        self.TEXT_SIZES = {
+            "huge": self.conf.get("text")["text_size_huge"],
+            "big": self.conf.get("text")["text_size_big"],
+            "mid": self.conf.get("text")["text_size_mid"],
+            "small": self.conf.get("text")["text_size_small"]
+        }
+
+        # Calling the parent constructor
         Canvas.__init__(self, self.master)
 
         # Drawing the screen
