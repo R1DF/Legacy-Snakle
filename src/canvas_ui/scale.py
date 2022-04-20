93dbf468b5f78397a441870dac16176fa35e3492
R1DF <33981824+R1DF@users.noreply.github.com>
2022-04-20 22:34:01 +0600

    developed scale more

--- src/canvas_ui/scale.py
+++ src/canvas_ui/scale.py
@@ -10,7 +10,7 @@ class Scale:
             master: Canvas,
             x,
             y,
-            number_field_offset_x,
+            number_rect_offset_x,
             offset_y,
             input_field_offset_x,
             theme,
@@ -36,25 +36,25 @@ class Scale:
 
         # Drawing
         self.number_rect = self.master.create_rectangle(
-            x - number_field_offset_x - input_field_offset_x,
+            x - number_rect_offset_x - input_field_offset_x,
             y - offset_y,
-            x + number_field_offset_x - input_field_offset_x,
+            x + number_rect_offset_x - input_field_offset_x,
             y + offset_y,
             fill=self.theme["scale_fill"]
         )
 
         self.up_input_field = self.master.create_rectangle(
-            x + number_field_offset_x - input_field_offset_x,
+            x + number_rect_offset_x - input_field_offset_x,
             y - offset_y,
-            x + number_field_offset_x + input_field_offset_x,
+            x + number_rect_offset_x + input_field_offset_x,
             y,
             fill=self.theme["scale_fill"]
         )
 
         self.down_input_field = self.master.create_rectangle(
-            x + number_field_offset_x - input_field_offset_x,
+            x + number_rect_offset_x - input_field_offset_x,
             y,
-            x + number_field_offset_x + input_field_offset_x,
+            x + number_rect_offset_x + input_field_offset_x,
             y + offset_y,
             fill=self.theme["scale_fill"]
         )
@@ -68,7 +68,7 @@ class Scale:
         )
 
         self.up_input_text = self.master.create_text(
-            x + number_field_offset_x,
+            x + number_rect_offset_x,
             y - offset_y // 2,
             text="\u2191",
             fill=self.theme["text_fill"],
@@ -76,7 +76,7 @@ class Scale:
         )
 
         self.down_input_text = self.master.create_text(
-            x + number_field_offset_x,
+            x + number_rect_offset_x,
             y + offset_y // 2,
             text="\u2193",
             fill=self.theme["text_fill"],
@@ -113,15 +113,60 @@ class Scale:
             self.master.itemconfig(self.down_input_field, fill=self.theme["scale_fill"])
 
     def handle_lclick(self, event):
-        if is_inside(event, self.master.bbox(self.up_input_field)):
-            pass
+        if is_inside(event, self.master.bbox(self.number_rect)):
+            self.is_highlighted = True
+            self.cycle_counter = 0
+            self.master.itemconfig(self.number_rect, outline=self.theme["scale_cursor"])
+
+        elif is_inside(event, self.master.bbox(self.up_input_field)):
+            self.is_highlighted = False
+            self.value = self.value + 1 if self.value != self.max_value else self.value
+            self.master.itemconfig(self.value_number_text, text=str(self.value))
+            self.master.itemconfig(self.number_rect, outline=self.theme["scale_border"])
+
+            self.callback_up()
 
         elif is_inside(event, self.master.bbox(self.down_input_field)):
-            pass
+            self.is_highlighted = False
+            self.value = self.value - 1 if self.value != self.min_value else self.value
+            self.master.itemconfig(self.value_number_text, text=str(self.value))
+            self.master.itemconfig(self.number_rect, outline=self.theme["scale_border"])
+
+            self.callback_down()
+
+        else:
+            self.master.itemconfig(self.number_rect, outline=self.theme["scale_border"])
+            self.is_highlighted = False
 
     def handle_type(self, event):
-        pass
+        if self.is_highlighted:
+            if event.keysym.isdecimal() or event.keysym == "BackSpace": # must be a number or a backspace
+                if event.char.isdigit():
+                    self.value = int(str(self.value) + event.char) if int(
+                        str(self.value) + event.char) <= self.max_value else self.value
+
+                if event.keysym == "BackSpace":
+                    if len(str(self.value)) > 1:
+                        self.value = int(str(self.value)[:-1]) if len(str(self.value)) > 1 else self.value
+                    elif len(str(self.value)) == 1:
+                        self.value = 0
+
+                    # Validity checking
+                    if self.value < self.min_value:
+                        self.master.itemconfig(self.value_number_text, fill=self.theme["text_invalid"])
+                    else:
+                        self.master.itemconfig(self.value_number_text, fill=self.theme["text_fill"])
+
+                self.master.itemconfig(self.value_number_text, text=str(self.value))
 
     def highlight_cycle(self):
-        pass
+        if self.is_highlighted:
+            self.cycle_counter = (self.cycle_counter + 1) % 2
+
+            if self.cycle_counter == 1:
+                self.master.itemconfig(self.number_rect, outline=self.theme["scale_cursor"])
+            else:
+                self.master.itemconfig(self.number_rect, outline=self.theme["scale_border"])
+
+        self.master.after(self.conf.get("widgets")["scale_highlight_cycle_ms"], self.highlight_cycle)
 
