aeeaaf504531444fb7a620d07b577812cc75c0ba
R1DF <33981824+R1DF@users.noreply.github.com>
2022-04-28 07:50:37 +0600

    added left frame for now

--- src/font_manager.py
+++ src/font_manager.py
@@ -12,6 +12,20 @@ class FontManager(Toplevel):
         self.geometry("600x400")
         self.protocol("WM_DELETE_WINDOW", self.handle_exit)
 
+        # Building widgets
+        self.left_frame = Frame(self)
+        self.left_frame.grid(row=0, column=0)
+
+        self.right_frame = Frame(self)
+        self.right_frame.grid(row=0, column=1)
+
+        # Left frame (fonts overview)
+        self.fonts_label = Label(self.left_frame, text="List of fonts:")
+        self.fonts_label.pack()
+
+        self.fonts_listbox = Listbox(self.left_frame, width=50, height=23)
+        self.fonts_listbox.pack(expand="1")
+
     def handle_exit(self):
         self.master.is_font_manager_open = False
         self.destroy()
