7c0667885096345e11dd54032374e44327362ee1
R1DF <33981824+R1DF@users.noreply.github.com>
2022-04-29 08:58:00 +0600

    added loading bar and also put the bug where a button doesn't highlight when you exit

--- src/pack_manager.py
+++ src/pack_manager.py
@@ -1,30 +1,83 @@
 # Imports
 from screen import *
 from canvas_ui.button import Button
+from canvas_ui.loading_bar import LoadingBar
 
 # Main menu canvas
 class PacksManager(Screen):
     def __init__(self, master, theme, conf):
         Screen.__init__(self, master, theme, conf)  # check out if this line is necessary
+        self.event = None # contains x and y mouse position
 
     def initiate(self):
         # Filling with theme
         self.config(bg=self.theme["bg"])
 
         # Drawing out widgets
+        self.intro_text = self.create_text(
+            self.WIDTH // 2,
+            50,
+            text="Packs",
+            font=[self.FONT, self.TEXT_SIZES["huge"]]
+        )
+
+        #Drawing out the loader
+        self.loader_intro = self.create_text(
+            self.WIDTH // 2,
+            (self.HEIGHT // 2) - 50,
+            text="Loading...",
+            font=[self.FONT, self.TEXT_SIZES["small"]]
+        )
+
+        self.loader_loading_bar = LoadingBar(
+            self,
+            self.WIDTH // 2,
+            self.HEIGHT // 2,
+            400,
+            40,
+            self.theme,
+            self.conf
+        )
+
+        self.create_line(  # Divider
+            0,
+            95,
+            self.WIDTH,
+            95,
+            fill=self.theme["line_fill"],
+            width=2
+        )
+
+        self.create_line(  # Divider
+            0,
+            505,
+            self.WIDTH,
+            505,
+            fill=self.theme["line_fill"],
+            width=2
+        )
+
         self.back_button = Button(
             self,
-            (self.WIDTH // 2) - 130,
-            540,
-            200,
-            70,
+            (self.WIDTH // 2),
+            550,
+            400,
+            60,
             text="Back to Settings",
             conf=self.conf,
             theme=self.theme,
             callback=self.return_to_settings
         )
 
+        # Binding a mouse hovering because if it clicks on the settings button, it needs to pass the event
+        self.bind("<Motion>", self.handle_motion, add="+")
+
     def return_to_settings(self):
         self.master.make_settings()
+        self.master.settings.back_button.handle_motion(self.event)
+        self.master.settings.save_changes_button.handle_motion(self.event)
         self.destroy()
+
+    def handle_motion(self, event):
+        self.event = event
 
