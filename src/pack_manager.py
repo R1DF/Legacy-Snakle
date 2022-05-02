21a1f2197f3b516db8f7f7443df9c93aafbfdc77
R1DF <33981824+R1DF@users.noreply.github.com>
2022-05-02 22:36:54 +0600

    still a wip, trying to implement the system with the loading bar

--- src/pack_manager.py
+++ src/pack_manager.py
@@ -2,6 +2,8 @@
 from screen import *
 from canvas_ui.button import Button
 from canvas_ui.loading_bar import LoadingBar
+from pack_finder import PackFinder
+from loading_sys import LoadingSystem
 
 # Main menu canvas
 class PacksManager(Screen):
@@ -12,8 +14,21 @@ class PacksManager(Screen):
     def initiate(self):
         # Filling with theme
         self.config(bg=self.theme["bg"])
+        self.pack_finder = PackFinder(self, "\\packs")
 
         # Drawing out widgets
+        self.draw_loading_part() # This draws out the loaders
+        self.loader_loading_bar.increment()
+        self.loader_loading_bar.increment()
+        self.loader_loading_bar.increment()
+
+        # Binding a mouse hovering because if it clicks on the settings button, it needs to pass the event
+        self.bind("<Motion>", self.handle_motion, add="+")
+
+        # Beginning the loading process
+        self.after(100, self.begin_loading)
+
+    def draw_loading_part(self):
         self.intro_text = self.create_text(
             self.WIDTH // 2,
             50,
@@ -37,7 +52,7 @@ class PacksManager(Screen):
             40,
             self.theme,
             self.conf,
-            max_value=7
+            max_value=self.pack_finder.get_amount_of_packs()
         )
 
         self.create_line(  # Divider
@@ -70,9 +85,9 @@ class PacksManager(Screen):
             callback=self.return_to_settings
         )
 
-        # Binding a mouse hovering because if it clicks on the settings button, it needs to pass the event
-        self.bind("<Motion>", self.handle_motion, add="+")
-        self.bind("<Button-1>", self.bar_test, add="+")
+    def begin_loading(self):
+        self.loading_system = LoadingSystem(self, self.loader_loading_bar, "\\packs")
+        self.loading_system.recur_start(0)
 
     def return_to_settings(self):
         self.master.make_settings()
@@ -82,7 +97,4 @@ class PacksManager(Screen):
 
     def handle_motion(self, event):
         self.event = event
-
-    def bar_test(self, event):
-        self.loader_loading_bar.increment(1)
 
