f4c617e38dd4e26236310aa03665e6ddf9993e88
R1DF <33981824+R1DF@users.noreply.github.com>
2022-05-12 13:25:21 +0600

    updated list selector, more functionaliy later

--- src/canvas_ui/pack_selector_list.py
+++ src/canvas_ui/pack_selector_list.py
@@ -1,6 +1,11 @@
 # Imports
 from tkinter import Canvas
+from .list_selector import ListSelector
 from .is_inside import is_inside
+from os import getcwd, listdir
+
+# PACKS PATH file
+PACKS_PATH = getcwd() +"\\packs\\"
 
 # Pack Selector List class
 class PackSelectorList:
@@ -19,6 +24,17 @@ class PackSelectorList:
         self.init_coordinates = (x-(offset_x//2), y-(offset_y//2), x+(offset_x//2), y+(offset_y//2))
         self.conf = conf
         self.theme = theme
+        self.packs = [x for x in listdir(PACKS_PATH) if x.split(".")[1] == "json"] # Only files with the JSOn extension in the "packs" directory are included
 
-        # Drawing
+        # Drawing rectangle
         self.rect = self.master.create_rectangle(*self.init_coordinates, width=2, fill=self.theme["selector_list_fill"])
+
+        # Drawing out page
+        self.draw_page(1)
+
+    def draw_page(self, number):
+        pass
+
+    def clear(self):
+        pass
+
