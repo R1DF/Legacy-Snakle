9d8fceb7e5021c2a88c663e112b55eeb5373770b
R1DF <33981824+R1DF@users.noreply.github.com>
2022-05-13 11:23:41 +0600

    added more functionality, page drawing incomplete and buggy

--- src/canvas_ui/pack_selector_list.py
+++ src/canvas_ui/pack_selector_list.py
@@ -1,6 +1,6 @@
 # Imports
 from tkinter import Canvas
-from .list_selector import ListSelector
+from .file_selector import FileSelector
 from .is_inside import is_inside
 from os import getcwd, listdir
 
@@ -22,9 +22,11 @@ class PackSelectorList:
         # Initialization
         self.master = master
         self.init_coordinates = (x-(offset_x//2), y-(offset_y//2), x+(offset_x//2), y+(offset_y//2))
-        self.conf = conf
+        self.x, self.y, self.offset_x, self.offset_y = x, y, offset_x, offset_y
         self.theme = theme
+        self.conf = conf
         self.packs = [x for x in listdir(PACKS_PATH) if x.split(".")[1] == "json"] # Only files with the JSOn extension in the "packs" directory are included
+        self._selector_items = []
 
         # Drawing rectangle
         self.rect = self.master.create_rectangle(*self.init_coordinates, width=2, fill=self.theme["selector_list_fill"])
@@ -33,8 +35,22 @@ class PackSelectorList:
         self.draw_page(1)
 
     def draw_page(self, number):
+        # Preparations before drawing
         self.clear() # Clears the canvas
         packs_of_page = self.packs[((number-1) * 4):(number * 4 if number * 4 <= len(self.packs) else len(self.packs))] # Used slicing to get the correct part of the packs list to draw out
+
+        # Drawing out
+        for o in range(len(packs_of_page)): # o ---> order of item
+            self._selector_items.append(FileSelector(
+                self,
+                self.x,
+                (self.y - self.offset_y) + (o * (self.offset_y/4)),
+                self.offset_x,
+                o * (self.offset_y / 4),
+                file_name=packs_of_page[o],
+                theme=self.theme,
+                conf=self.conf
+            ))
 
     def clear(self):
         pass
