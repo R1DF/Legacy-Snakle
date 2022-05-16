d3c024024732112861a6594a04da2d24beec7889
R1DF <33981824+R1DF@users.noreply.github.com>
2022-05-16 11:25:00 +0600

    updated; more updates later

--- src/canvas_ui/file_selector.py
+++ src/canvas_ui/file_selector.py
@@ -1,3 +1,10 @@
+## TODO: rename class to PackSelector, ThemeSelector will have something else
+
+# Imports
+import json  # For packs
+import os
+
+# File Selector class
 class FileSelector:
     def __init__(
             self,
@@ -13,9 +20,12 @@ class FileSelector:
         # Initialization
         self.master = master
         self.init_coordinates = (x - (offset_x // 2), y - (offset_y // 2), x + (offset_x // 2), y + (offset_y // 2))
+        self.x, self.y = x, y
         self.file_name = file_name
         self.theme = theme
         self.conf = conf
+        self.text_data = conf.get("text")
+        self.file_title = ""
 
         # Drawing
         self.rect = self.master.master.create_rectangle(
@@ -23,3 +33,20 @@ class FileSelector:
             fill=self.theme["selector_element_fill"],
             width=2
         )
+
+        # Displaying data
+        self.configure_display()
+
+    def configure_display(self):
+        # Getting file data
+        self.file = json.load(open(os.getcwd() + "\\packs\\" + self.file_name, "r"))
+
+        # Displays
+        self.pack_title_text = self.master.master.create_text(
+            self.x - 70, #self.init_coordinates[0] + (len(self.file["title"][:20] * 6) * 1.1),
+            self.init_coordinates[1] + 20,
+            text=self.file["title"] if len(self.file["title"]) <= 20 else self.file["title"][:20]+"...",
+            font=[self.master.master.FONT, self.text_data["text_size_mid"]],
+            justify="left"
+        )
+
