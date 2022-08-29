e4142e308d71606985b99b22401ff196aa539a5f
R1DF <33981824+R1DF@users.noreply.github.com>
2022-08-29 16:46:55 +0600

    finally complete

--- src/canvas_ui/file_selector.py
+++ src/canvas_ui/file_selector.py
@@ -1,2 +1,114 @@
+# Imports
+import os
+import json
+import toml
+from .is_inside import is_inside
+
+
+# File Selector class
 class FileSelector:
-    pass
+    def __init__(
+            self,
+            master,
+            x,
+            y,
+            offset_x,
+            offset_y,
+            file_name,
+            theme,
+            conf,
+            file_type
+    ):
+        # Initialization
+        # Widget attributes
+        self.master = master
+        self.init_coordinates = (x - (offset_x // 2), y - (offset_y // 2), x + (offset_x // 2), y + (offset_y // 2))
+        self.x, self.y = x, y
+        self.theme = theme
+        self.conf = conf
+        self.file_type = file_type
+
+        # Meta and special attributes
+        self.file_name = file_name
+        self.text_data = conf.get("text")
+        self.file_title = ""
+        self.is_selected = False
+
+        # Drawing
+        self.rect = self.master.master.create_rectangle(
+            *self.init_coordinates,
+            fill=self.theme["selector_element_fill"],
+            width=2
+        )
+
+        # Displaying data
+        self.configure_display()
+
+        # Binding
+        self.master.master.bind("<Motion>", self.handle_motion, add="+")
+
+    def configure_display(self):
+        # Getting file type
+        if self.file_type == "pack":
+            self.file = json.load(open(os.getcwd() + "\\packs\\" + self.file_name, "r"))
+            self.file_title = self.file["title"]
+            self.file_date_created = self.file["dateCreated"]
+            self.file_author = self.file["creator"]
+
+        elif self.file_type == "theme":
+            self.file = toml.load(os.getcwd() + "\\themes\\" + self.file_name)
+            self.file_title = self.file["meta"]["name"]
+            self.file_date_created = self.file["meta"]["date"]
+            self.file_author = self.file["meta"]["author"]
+
+        # Displays
+        self.file_title_text = self.master.master.create_text(
+            self.init_coordinates[0] + 140,
+            self.init_coordinates[1] + 20,
+            text=self.file_title if len(self.file_title) <= 20 else self.file_title[:20] + "...",
+            font=[self.master.master.FONT, self.text_data["text_size_mid"]],
+            justify="left"
+        )
+
+        self.file_date_text = self.master.master.create_text(
+            self.init_coordinates[0] + 80,
+            # might use proportions to figure out how long the X offset should be at line 46
+            self.init_coordinates[3] - 20,
+            text=self.file_date_created,
+            font=[self.master.master.FONT, self.text_data["text_size_mid"]],
+            justify="right"
+        )
+
+        self.file_creator_text = self.master.master.create_text(
+            self.init_coordinates[2] - 50,
+            # might use proportions to figure out how long the X offset should be at line 46
+            self.init_coordinates[3] - 20,
+            text=self.file_author,
+            font=[self.master.master.FONT, self.text_data["text_size_mid"]],
+            justify="right"
+        )
+
+    def handle_motion(self, event):
+        if is_inside(event, self.init_coordinates):
+            if not self.is_selected:
+                self.master.master.itemconfig(self.rect, fill=self.theme["selector_element_highlight"])
+        else:
+            if not self.is_selected:
+                self.master.master.itemconfig(self.rect, fill=self.theme["selector_element_fill"])
+            else:
+                self.master.master.itemconfig(self.rect, fill=self.theme["selector_element_selected"])
+
+    def select(self):
+        self.is_selected = True
+        self.master.master.itemconfig(self.rect, fill=self.theme["selector_element_selected"])
+
+    def deselect(self):
+        self.is_selected = False
+        self.master.master.itemconfig(self.rect, fill=self.theme["selector_element_fill"])
+
+    def kill(self):
+        self.is_selected = False
+        self.master.master.delete(self.rect)
+        self.master.master.delete(self.file_title_text)
+        self.master.master.delete(self.file_date_text)
+        self.master.master.delete(self.file_creator_text)
--- src/canvas_ui/file_selector_list.py
+++ src/canvas_ui/file_selector_list.py
@@ -17,6 +17,7 @@ class FileSelectorList():
             overlooked_directory,
             conf,
             theme,
+            file_type,
             extension="toml",
             additional_callback=lambda: None,
             callback_upon_selected_click=lambda: None # the function that runs when you click an already selected file
@@ -29,13 +30,18 @@ class FileSelectorList():
         self.theme = theme
         self.conf = conf
         self.directory_elements = [x for x in listdir(self.directory_path) if x.split(".")[1] == self.extension]
+        self.x, self.y = x, y
+        self.file_type = file_type # for FileSelectors
 
         # Functionality variables
         self.callback = additional_callback
         self.selected_callback = callback_upon_selected_click
         self.current_page = 1
-        self.max_page_amount = ceil(len(self.directory_path)/4)
+        self.max_page_amount = ceil(len(self.directory_elements)/4)
         self.selected_selector = None
+        self.selectors = []
+        self.offset_x = self.init_coordinates[2] - self.init_coordinates[0]
+        self.offset_y = self.init_coordinates[3] - self.init_coordinates[1]
 
         # Drawing out main rectangle and page
         self.rect = self.master.create_rectangle(*self.init_coordinates, width=2, fill=self.theme["selector_list_fill"])
@@ -45,7 +51,28 @@ class FileSelectorList():
         self.master.bind("<Button-1>", self.handle_lclick, add="+")
 
     def draw_page(self, page_number):
-        pass
+        self.clear()
+        packs = self.directory_elements[((page_number-1) * 4):(page_number * 4 if page_number * 4 <= len(self.directory_elements) else len(self.directory_elements))]
+        self.current_page = page_number
+
+        # Drawing out
+        for o in range(len(packs)): # o ---> order of item
+            self.selectors.append(FileSelector(
+                self,
+                self.x,
+                self.master.bbox(self.rect)[1] + ((self.offset_y / 8) * ((2 * o) + 1)),  # 2n+1
+                self.offset_x,
+                self.offset_y / 4,
+                file_name=packs[o],
+                theme=self.theme,
+                conf=self.conf,
+                file_type=self.file_type
+            ))
+
+    def clear(self):
+        for selector in self.selectors:
+            selector.kill()
+        self.selectors = []
 
     def select(self, selector: FileSelector):
         if self.selected_selector is not None:
@@ -59,5 +86,18 @@ class FileSelectorList():
             self.selected_selector = None
 
     def handle_lclick(self, event):
-        pass
+        # Checking if a selected selector was selected again
+        if self.selected_selector is not None:
+            if is_inside(event, self.master.bbox(self.selected_selector.rect)):
+                self.selected_callback()
+                return
+
+        # Otherwise...
+        for selector in self.selectors:
+            if is_inside(event, self.master.bbox(selector.rect)):
+                self.select(selector)
+                break
+        else:  # yeah I use for-elses
+            self.nullify_selection()
+        self.callback()
 
