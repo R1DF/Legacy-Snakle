cfec8a16f229df3386dc38cd92f1d06f99479106
R1DF <33981824+R1DF@users.noreply.github.com>
2022-08-30 13:24:21 +0600

    set up game making screen (final functionality TBA)

--- src/gameinitmenu.py
+++ src/gameinitmenu.py
@@ -4,41 +4,156 @@ from canvas_ui.button import Button
 from canvas_ui.file_selector_list import FileSelectorList
 from necessary_defaults import THEMES_PATH, DEFAULT_THEME
 from os import getcwd
+from key_edit_window import URLConfigurator
 
 # Game initialization canvas
 class GameInitMenu(Screen):
     def __init__(self, master, theme, conf):
         Screen.__init__(self, master, theme, conf)
+        self.opened_url_configurator = False
 
     def initiate(self):
         # Filling with theme
         self.config(bg=self.theme["bg"])
 
         # Drawing out widgets
-        self.intro_text = self.create_text(
-            self.WIDTH // 2,
-            50,
-            text="Play",
-            font=[self.FONT, self.TEXT_SIZES["huge"]]
-        )
-        "Select Online"
         self.create_text(
             self.WIDTH // 2,
-            110,
-            text="Select the pack you'd like to use:",
+            30,
+            text="Select offline pack:",
             font=[self.FONT, self.TEXT_SIZES["mid"]]
         )
 
         self.pack_selector_list = FileSelectorList(
             self,
             self.WIDTH // 2,
-            self.HEIGHT // 2,
+            210,
             425,
             300,
             overlooked_directory= getcwd() + "\\packs\\",
             conf=self.conf,
             theme=self.theme,
             extension="json",
-            file_type="pack"
+            file_type="pack",
+            additional_callback=self.synchronize_selector
         )
 
+        self.selected_pack_shower = self.create_text(
+            (self.WIDTH // 2) - 120,
+            400,
+            text="Selected pack:\nN/A",
+            font=[self.FONT, self.TEXT_SIZES["mid"]],
+            justify="center"
+        )
+
+        self.previous_page_button = Button(
+            self,
+            (self.WIDTH // 2) + 40,
+            400,
+            40,
+            40,
+            text="<",
+            theme=self.theme,
+            conf=self.conf,
+            callback=self.previous_page
+        )
+
+        self.page_shower = self.create_text(
+            (self.WIDTH // 2) + 120,
+            400,
+            text="Page 1",
+            font=[self.FONT, self.TEXT_SIZES["mid"]],
+        )
+
+        self.next_page_button = Button(
+            self,
+            (self.WIDTH // 2) + 200,
+            400,
+            40,
+            40,
+            text=">",
+            theme=self.theme,
+            conf=self.conf,
+            callback=self.next_page
+        )
+
+        self.create_line(  # Divider
+            0,
+            440,
+            self.WIDTH,
+            440,
+            fill=self.theme["line_fill"],
+            width=2
+        )
+
+        self.modify_words_url_button = Button(
+            self,
+            self.WIDTH // 2,
+            480,
+            450,
+            50,
+            text="Play with URL (requires internet)",
+            theme=self.theme,
+            conf=self.conf,
+            callback=self.open_url_configurator
+        )
+
+        self.back_button = Button(
+            self,
+            self.WIDTH // 2 - 125,
+            550,
+            200,
+            60,
+            text="Back",
+            theme=self.theme,
+            conf=self.conf,
+            callback=self.return_to_main_menu
+        )
+
+        self.play_offline_button = Button(
+            self,
+            self.WIDTH // 2 + 125,
+            550,
+            200,
+            60,
+            text="Play (offline)",
+            theme=self.theme,
+            conf=self.conf
+        )
+
+    def return_to_main_menu(self):
+        self.master.make_main_menu()
+        self.destroy()
+
+    def synchronize_selector(self):
+        if self.pack_selector_list.selected_selector is not None:
+            self.itemconfig(
+                self.selected_pack_shower,
+                text=f"Selected pack:\n{self.pack_selector_list.selected_selector.file_title[:17]+'...'if len(self.pack_selector_list.selected_selector.file_title) > 17 else self.pack_selector_list.selected_selector.file_title}"
+        )
+        else:
+            self.itemconfig(self.selected_pack_shower, text="Selected pack:\nN/A")
+
+    def next_page(self):
+        if self.pack_selector_list.current_page < self.pack_selector_list.max_page_amount:
+            # Drawing out the selectors
+            self.pack_selector_list.clear()
+            self.pack_selector_list.current_page += 1
+            self.pack_selector_list.draw_page(self.pack_selector_list.current_page)
+
+            # Modifying the page text
+            self.itemconfig(self.page_shower, text=f"Page {self.pack_selector_list.current_page}")
+
+    def previous_page(self):
+        if self.pack_selector_list.current_page > 1:
+            # Drawing out the selectors
+            self.pack_selector_list.clear()
+            self.pack_selector_list.current_page -= 1
+            self.pack_selector_list.draw_page(self.pack_selector_list.current_page)
+
+            # Modifying the page text
+            self.itemconfig(self.page_shower, text=f"Page {self.pack_selector_list.current_page}")
+
+    def open_url_configurator(self):
+        if not self.opened_url_configurator:
+            self.url_configurator = URLConfigurator(self)
