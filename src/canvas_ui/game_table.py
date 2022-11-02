3a3d540b83dbf1ffaef1ea324b4e369830624c61
R1DF <33981824+R1DF@users.noreply.github.com>
2022-11-02 20:32:14 +0600

    added game table class and did the hardest piece of math probably for this project

--- src/canvas_ui/game_table.py
+++ src/canvas_ui/game_table.py
@@ -2,13 +2,18 @@
 from tkinter import Canvas
 from .is_inside import is_inside
 
-# Button class
-class Button:
+# Box class
+class Box:
+    def __init__(self, master, x, y):
+        pass
+
+# Table class
+class WordTable:
     def __init__(
             self,
             master: Canvas,
             x,
-            y,
+            starting_y,
             offset,
             gap_x,
             conf,
@@ -16,11 +21,28 @@ class Button:
     ):
         # Initialization
         self.master = master
-        self.center_x, self.center_y = x, y
+        self.center_x, self.start_y = x, starting_y  # start_y is the y position of the squares on the first row
         self.square_width = offset
         self.gap_length = gap_x
         self.conf = conf
         self.theme = theme
         self.width = (5 * self.square_width) + (4 * self.gap_length)
-        self.length = (6 * self.square_width) + (5 * self.gap_length)
+        self.height = (6 * self.square_width) + (5 * self.gap_length)
+
+        # Drawing
+        self.boxes = []
+        for y_index in range(1, 7):
+            row = []
+            for x_index in range(5):
+                # WARNING: LOOKING AT THE CODE BELOW MIGHT MAKE YOU LOSE YOUR SANITY.
+                row.append(self.master.create_rectangle(
+                    (x_index * self.square_width) + (x_index * self.gap_length) + (self.center_x - (self.width // 2)),
+                    (y_index * self.square_width) + (y_index * self.gap_length) + (self.center_x - (self.height // 2)) + (self.start_y - (self.height // 2)),
+                    (x_index * self.square_width) + (x_index * self.gap_length) + (self.center_x - (self.width // 2)) + self.square_width,
+                    (y_index * self.square_width) + (y_index * self.gap_length) + (self.center_x - (self.height // 2)) + (self.start_y - (self.height // 2)) + self.square_width,
+                    width=2,
+                    fill=self.theme["grid_square_fill"],
+                    outline=self.theme["grid_square_border"]
+                ))
+            self.boxes.append(row)
 
