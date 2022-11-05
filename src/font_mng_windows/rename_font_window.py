740166fbea32f916ec1409997854073bcce3d978
R1DF <33981824+R1DF@users.noreply.github.com>
2022-11-05 22:46:29 +0600

    conventional titling + reformat

--- src/font_mng_windows/rename_font_window.py
+++ src/font_mng_windows/rename_font_window.py
@@ -1,14 +1,15 @@
 # Imports
 from tkinter import Toplevel, Frame, Label, Entry, Button, font, messagebox
 
+
 # Making the adder window
 class FontRenamer(Toplevel):
     def __init__(self, master, font_index):
         # Initialization
         Toplevel.__init__(self, master)
         self.master = master
-        self.title("Rename font")
-        self.font_index = font_index # This value is the index of the selected font in the list
+        self.title("Rename")
+        self.font_index = font_index  # This value is the index of the selected font in the list
         self.master.opened_window_parameters["rename"] = True
         self.resizable(False, False)
 
@@ -40,12 +41,12 @@ class FontRenamer(Toplevel):
             messagebox.showerror("Empty input", "Please enter a font.")
         elif self.new_font_name_entry.get().strip().lower() not in [x.lower() for x in font.families()]:
             messagebox.showerror("Invalid font", "The entered font does not exist.")
-        elif self.new_font_name_entry.get().strip().lower() in [x.lower() for x in self.master.fonts_listbox.get(0, "end")]: # same case in add_font_window.py
+        elif self.new_font_name_entry.get().strip().lower() in [x.lower() for x in self.master.fonts_listbox.get(0, "end")]:  # same case in add_font_window.py
             messagebox.showerror("Font already used", "You already have this font used.")
         else:
             font_position_in_list = [x.lower() for x in font.families()].index(self.new_font_name_entry.get().strip().lower())
             self.master.fonts_listbox.delete(self.font_index)
-            self.master.fonts_listbox.insert(self.font_index, font.families()[font_position_in_list]) # same case here too just like in add_font_window.py
+            self.master.fonts_listbox.insert(self.font_index, font.families()[font_position_in_list])  # same case here too just like in add_font_window.py
             self.handle_exit()
 
     def handle_exit(self):
