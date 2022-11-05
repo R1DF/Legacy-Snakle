52b90f28b6d9a1f8d05c3aa3a8610a923dffb6d8
R1DF <33981824+R1DF@users.noreply.github.com>
2022-11-05 13:48:36 +0600

    fixed logic error to do with percentages + reformat

--- src/clearance_checker.py
+++ src/clearance_checker.py
@@ -18,16 +18,16 @@ class ClearanceChecker:
     def __init__(self, packs_path):
         # Initialization
         self.packs_path = packs_path
-        self.clearances_path = os.getcwd()+"\\clearance_data\\"
+        self.clearances_path = os.getcwd() + "\\clearance_data\\"
 
     def compare_progress(self, pack):
         """
         This function will compare a pack's clearance by dividing the amount of clear words in the clearance file and the
-        amount of words in the pack file.
+        amount of words in the pack file. The percentage of completion will be returned.
         """
         clear_words = json.load(open(self.clearances_path+f"c_{pack}", "r"))["clear_words"]
         all_words = json.load(open(self.packs_path+pack, "r"))["words"]
-        return round(len(clear_words) / len(all_words), 2)
+        return round(len(clear_words) / len(all_words), 2) * 100
 
     def check_files(self):
         """
